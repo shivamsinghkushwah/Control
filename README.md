be65ffae0a8da7604f620343bbd4ffa70777ab34
Shivam Singh <shivamsinghkushwah@yahoo.com>
2020-02-05 11:47:05 +0530

    Firmware update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 "# Control" 
 "# Control" 
+"# Control" 
---
+++ workspace_v9_2/flashapi_ex3_live_firmware_update/BANK0_FLASH/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../28004x_flash_api_liveFirmwareUpdate_bank0_lnk.cmd 
+
+LIB_SRCS += \
+../F021_API_F28004x_FPU32.lib \
+C:/ti/c2000/C2000Ware_MotorControl_SDK_2_01_00_00/c2000ware/driverlib/f28004x/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../flashapi_ex3_live_firmware_update.c 
+
+C_DEPS += \
+./flashapi_ex3_live_firmware_update.d 
+
+OBJS += \
+./flashapi_ex3_live_firmware_update.obj 
+
+OBJS__QUOTED += \
+"flashapi_ex3_live_firmware_update.obj" 
+
+C_DEPS__QUOTED += \
+"flashapi_ex3_live_firmware_update.d" 
+
+C_SRCS__QUOTED += \
+"../flashapi_ex3_live_firmware_update.c" 
+
+
